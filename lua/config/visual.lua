3951b724f9913bbdb467f72f5c5ec277ff939fea
OceanMan156 <delgadad@sunypoly.edu>
2025-01-13 15:01:18 -0500

    Lazy Setup:
      - Made background transparent

--- lua/config/visual.lua
+++ lua/config/visual.lua
@@ -10,3 +10,12 @@ vim.opt.smartindent = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.scrolloff = 20
 vim.opt.cursorline = true
+vim.opt.splitright = true
+
+-- transparent background
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
