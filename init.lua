7459b2d2d9e7b1f6c8a32b1f3bed763e307c43de
Miguel Delgado <mdelgado@albany.k12.ny.us>
2025-06-20 09:41:13 -0400

    added git-fugitive and updated visual options in init.lua

--- init.lua
+++ init.lua
@@ -3,13 +3,23 @@ require("config.options")
 require("config.mappings")
 require("config.lsp-config")
 
--- Plugins
-
 -- Feel
 o = vim.opt
+w = vim.wo
 
 o.number = true
 o.relativenumber = true
+w.wrap = false
+w.colorcolumn = '80'
+o.expandtab = true
+o.autoindent = true
+o.tabstop = 2
+o.shiftwidth = 2
+o.smartindent = true
+o.scrolloff = 20
+o.cursorline = true
+o.splitright = true
 
 o.signcolumn = "number"
 o.clipboard = "unnamedplus"
+
